366477cac0b39e4f2225a3d1648ade44ba4bd05d
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-26 23:55:04 -0500

    create (and protecting) user dashboard

---
+++ src/app/[user]/page.tsx
@@ -0,0 +1,29 @@
+import { getUserNameByEmail } from "@/server/actions/user";
+import { getServerSession } from "next-auth";
+
+const DashboardPage = async ({
+  params: { user },
+}: {
+  params: { user: string };
+}) => {
+  const session = await getServerSession();
+  if (!session || !session.user || !session.user.email)
+    return <div>Go back home page</div>;
+
+  const data = await getUserNameByEmail(session.user.email);
+  if (!data.success) return <div>Go back home page</div>;
+
+  const {
+    result: { name },
+  } = data;
+
+  if (name !== user) return <div>Go back home page</div>;
+
+  return (
+    <main>
+      <h1>Dashboard</h1>
+    </main>
+  );
+};
+
+export default DashboardPage;
