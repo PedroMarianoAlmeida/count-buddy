f74c342367dcbb7a6f52affaa1a37a4f83103839
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-28 21:32:03 -0500

    fix header menu on top (and adding a div with same heigh)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,18 +4,19 @@ import { Button } from "@/components/ui/button";
 import Menu from "./Menu";
 
 const Navbar = () => (
-  <header className="flex justify-between items-center px-4 md:px-6 lg:px-8 bg-secondary">
-    <div >
-      <Link href="/">
-        <Button variant="ghost">
-          Count Buddy
-        </Button>
-      </Link>
-    </div>
-    <div className="flex-none">
-      <Menu />
-    </div>
-  </header>
+  <>
+    <header className="flex justify-between items-center px-4 md:px-6 lg:px-8 bg-secondary fixed top-0 w-full z-10 h-14">
+      <div>
+        <Link href="/">
+          <Button variant="ghost">Count Buddy</Button>
+        </Link>
+      </div>
+      <div className="flex-none">
+        <Menu />
+      </div>
+    </header>
+    <div className="h-14" />
+  </>
 );
 
 export default Navbar;
