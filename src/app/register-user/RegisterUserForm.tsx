179498c96829b26cc0bfb39767cc83d846bbc345
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-29 21:36:39 -0500

    sanitize username

--- src/app/register-user/RegisterUserForm.tsx
+++ src/app/register-user/RegisterUserForm.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import { urlFormatter } from "@/utils/text";
 import {
   Form,
   FormControl,
@@ -14,17 +16,27 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  name: z.string().min(3), //.refine check BD,
-});
-
 const RegisterUserForm = () => {
+  const [message, setMessage] = useState("");
+  const [nameTreated, setNameTreated] = useState("");
+
+  const formSchema = z.object({
+    name: z.string().min(3), //.refine check BD,
+  });
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
     },
   });
+  const rawInput = form.watch("name");
+  useEffect(() => {
+    const nameTreated = urlFormatter(rawInput);
+    setNameTreated(nameTreated);
+
+    if (rawInput !== nameTreated) setMessage(`Name treated: ${nameTreated}`);
+  }, [rawInput]);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // const { description, amount } = values;
@@ -43,11 +55,10 @@ const RegisterUserForm = () => {
               <FormControl>
                 <Input placeholder="my-name" className="max-w-52" {...field} />
               </FormControl>
-              <FormMessage />
+              <FormMessage>{message}</FormMessage>
             </FormItem>
           )}
         />
-
         <Button type="submit">Register</Button>
       </form>
     </Form>
